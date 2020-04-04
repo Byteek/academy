ea2cc44e2888dc58188480616341cffcc0025f79
Byteek <byteek.p.n@gmail.com>
2020-04-04 21:41:55 +0300

    Lesson2

---
+++ academy/src/by/academy/lesson2/MaxMin.java
@@ -0,0 +1,35 @@
+package by.academy.lesson2;
+
+import java.util.Scanner;
+
+public class MaxMin {
+	
+	public static void main (String[] args) {
+	
+//	Scanner scan = new Scanner(System.in);
+//	String oneNumStr = scan.nextLine();
+//	String thoNumStr = scan.nextLine();
+		
+		int x = 1;
+		int y = 2;
+		
+		if (x > y) {
+			System.out.println(x + " Больше " + y);
+		}else {
+			System.out.println(y + " Больше " + x);
+		}
+		double sredn = 0;
+		sredn = ((x + y) / 2);
+		System.out.println("Среднее арифметическое = " +  sredn);
+		
+	
+	
+	
+	
+	
+	
+		
+		
+		
+	}
+}
---
+++ academy/src/by/academy/lesson2/NumberMonth.java
@@ -0,0 +1,53 @@
+package by.academy.lesson2;
+
+public class NumberMonth {
+
+	public static void main (String[] args) {
+		
+		int numberMonth = 678;
+		
+		switch (numberMonth) {
+			case 1:
+				System.out.println("Январь");
+				break;
+			case 2:
+				System.out.println("Февраль");
+				break;
+			case 3:
+				System.out.println("Март");
+				break;
+			case 4:
+				System.out.println("Апрель");
+				break;
+			case 5:
+				System.out.println("Май");
+				break;
+			case 6:
+				System.out.println("Июнь");
+				break;
+			case 7:
+				System.out.println("Июль");
+				break;
+			case 8:
+				System.out.println("Август");
+				break;
+			case 9:
+				System.out.println("Сентябрь");
+				break;
+			case 10:
+				System.out.println("Октябрь");
+				break;
+			case 11:
+				System.out.println("Ноябрь");
+				break;
+			case 12:
+				System.out.println("Декабрь");
+				break;
+			default:
+				System.out.println("Такого месяца не существует!");
+				break;
+		}
+		
+	}
+	
+}
