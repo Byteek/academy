08545ed11abaabb724e14ae75c755b227d2e8330
Nikita Pohodski <byteek.p.n@gmail.com>
2020-04-10 12:06:37 +0300

    second task complited

--- academyCourses/src/by/academy/homework/SecondTask.java
+++ academyCourses/src/by/academy/homework/SecondTask.java
@@ -21,13 +21,38 @@ public class SecondTask {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Введите тип данных ");
 		String type = scan.nextLine();
-		System.out.print("Введите значение переменной ");
-		String unknownValue = scan.nextLine();
+		
 		
 		switch (type) {
 			case "int": 
-				int valueInt = Integer.parseInt(unknownValue);
+				System.out.print("Введите значение переменной ");
+				int valueInt = scan.nextInt();
 				System.out.println("Остаток от деления " + valueInt + " на 2 = " + valueInt % 2);
+				break;
+			case "double":
+				System.out.print("Введите значение переменной ");
+				double valueDouble = scan.nextDouble();
+				System.out.println("70% от числа " + valueDouble + " = " + (valueDouble * 0.70));
+				break;
+			case "float":
+				System.out.print("Введите значение переменной ");
+				float valueFloat = scan.nextFloat();
+				System.out.println(valueFloat + " в квадрате = " + valueFloat * valueFloat);
+				break;
+			case "char":
+				System.out.print("Введите значение переменной ");
+				String unknownValue = scan.nextLine();
+				char valueChar = unknownValue.charAt(unknownValue.length()-1);
+				System.out.println("Код символа = " + (int)valueChar);
+				break;
+			case "String":
+				System.out.print("Введите значение переменной ");
+				String valueString = scan.nextLine();
+				System.out.println("Hello " + valueString);
+				break;
+			default:
+				System.out.println("Unsupported type");
+				break;
 		}
 		
 	}
