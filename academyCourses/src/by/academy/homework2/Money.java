77565ade903b369f4b589a750d9677012a3da955
Nikita Pohodski <byteek.p.n@gmail.com>
2020-04-18 20:54:58 +0300

    2

--- academyCourses/src/by/academy/homework2/Money.java
+++ academyCourses/src/by/academy/homework2/Money.java
@@ -1,6 +1,6 @@
 package by.academy.homework2;
 
-import java.text.DecimalFormat;
+import java.math.BigDecimal;
 
 public class Money {
 	private long rubli;
@@ -30,6 +30,21 @@ public class Money {
 		return new Money(allMoney / 100);
 	}
 
+	public Money addMoney(Money money, double plusMoney) { // сложение суммы с числом
+		long rubli = money.getRubli() + (long) plusMoney;
+		int cents = money.getCents() + (int) ((plusMoney * 100) % 100);
+		if (cents > 99) {
+			rubli++;
+			cents -= 100;
+		}
+		if (cents < 0) {
+			rubli--;
+			cents += 100;
+		}
+		double allMoney = ((rubli * 100 + (byte) cents));
+		return new Money(allMoney / 100);
+	}
+
 	public Money subtrMoney(Money money, Money minusMoney) { // вычитание сумм
 		long rubli = money.getRubli() - minusMoney.getRubli();
 		int cents = money.getCents() - minusMoney.getCents();
@@ -45,15 +60,18 @@ public class Money {
 		return new Money(allMoney / 100);
 	}
 
-//	public Money multiMoney(Money money, Money multiplyMoney) { // умножение сумм денег
-//		double sum = money * multiplyMoney;
-//
-//	}
-//
-//	public Money multiMoney(Money money, double factor) { // умножение суммы денег на дробно число
-//		double sum = money * multiplyMoney;
-//
-//	}
+	public Money multiMoney(Money money, Money multiplyMoney) {// умножение сумм денег
+		long fullMoney = ((money.getRubli() * 100) + money.getCents());
+		long fullDivMoney = ((multiplyMoney.getRubli() * 100) + multiplyMoney.getCents());
+		double result = (double) fullMoney * (double) fullDivMoney;
+		return new Money(result / 10000);
+	}
+
+	public Money multiMoney(Money money, double factor) { // умножение суммы денег на дробно число
+		long fullMoney = ((money.getRubli() * 100) + money.getCents());
+		double result = (double) (fullMoney) * factor;
+		return new Money(result / 100);
+	}
 
 	public Money divMoney(Money money, Money divMoney) { // деление суммы денег на сумму денег
 		long fullMoney = ((money.getRubli() * 100) + money.getCents()) * 100;
