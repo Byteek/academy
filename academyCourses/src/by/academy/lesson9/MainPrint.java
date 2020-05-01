bc5262a4f808e9f5370675b46b4c050c1e1b98cb
Nikita_Pohodski <byteek.p.n@gmail.com>
2020-05-01 18:14:10 +0300

    lesson9

--- academyCourses/src/by/academy/lesson9/MainPrint.java
+++ academyCourses/src/by/academy/lesson9/MainPrint.java
@@ -3,16 +3,18 @@ package by.academy.lesson9;
 public class MainPrint {
 
 	public static void main(String[] args) {
-		
+
 		Printable p = new Printable() {
 
 			@Override
 			public void print() {
-				
-				
+
+				System.out.println("Print!");
 			}
-		
-		}
+
+		};
+
+		p.print();
 
 	}
 
